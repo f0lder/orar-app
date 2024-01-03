244269a5ed9e69a34279e00c03f17be690a8ff7e
f0lder <ursanbogdan01@gmail.com>
2024-01-04 01:33:59 +0200

    make drag and drop orar

--- public/javascripts/drag.js
+++ public/javascripts/drag.js
@@ -1,10 +1,44 @@
 
 var el = document.getElementById('list1');
 var sortable1 = Sortable.create(el, {
-    group: 'shared',
+    group:{
+        name: 'shared',
+        pull: 'clone',
+    },
     animation: 150,
+    sort: false,
 });
-var sortable2 = Sortable.create(document.getElementById('list2'), {
-    group: 'shared',
-    animation: 150,
-});
+
+
+// var sortable3 = Sortable.create(document.getElementById('list3'), {
+//     group: {
+//         name: 'shared',
+//         pull: true,
+//     },
+//     animation: 150,
+//     onAdd: function (evt) {
+//         if(evt.to.children.length > 1){
+//             evt.item.remove();
+//         }
+//     },
+//     onRemove: function (evt) {    
+//         evt.item.remove();
+//      }
+// });
+
+var elements = document.getElementsByClassName('sortable');
+
+for (var i = 0; i < elements.length; i++) {
+    new Sortable(elements[i], {
+        group: {
+            name: 'shared',
+            pull: true,
+        },
+        animation: 150,
+        onAdd: function (evt) {
+            if(evt.to.children.length > 1){
+                evt.item.remove();
+            }
+        },
+    });
+}
--- routes/orar.js
+++ routes/orar.js
@@ -2,10 +2,23 @@ var express = require("express");
 const mongoose = require("mongoose");
 const { all } = require(".");
 
+const materii = require("../models").materii;
+const grupe = require("../models").grupe;
+
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('createOrar');
+router.get('/',async function (req, res, next) {
+    if (req.session.loggedin) {
+
+        let data =await materii.find({});
+
+        let g = await grupe.find({});
+
+        res.render('createOrar', { title: 'Orar', materii: data, grupe: g });
+
+    } else {
+        res.render('index', { redirected: true, title: "Orar-app" });
+    }
 });
 
 module.exports = router;

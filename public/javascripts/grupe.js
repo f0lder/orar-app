2c4ca6eed12f019ce1c355b5519a8db167579f6d
f0lder <ursanbogdan01@gmail.com>
2024-01-14 19:14:12 +0200

    details

--- public/javascripts/grupe.js
+++ public/javascripts/grupe.js
@@ -1,6 +1,6 @@
 
 
-validateForm("send",["cod","nSemigrupe","nStudenti"]);
-validateinput('cod',['cod','nSemigrupe','nStudenti'],'send');
-validateinput('nSemigrupe',['cod','nSemigrupe','nStudenti'],'send');
-validateinput('nStudenti',['cod','nSemigrupe','nStudenti'],'send');
+validateForm("send", ["cod", "nSemigrupe", "nStudenti"]);
+validateinput('cod', ['cod', 'nSemigrupe', 'nStudenti'], 'send');
+validateinput('nSemigrupe', ['cod', 'nSemigrupe', 'nStudenti'], 'send');
+validateinput('nStudenti', ['cod', 'nSemigrupe', 'nStudenti'], 'send');
